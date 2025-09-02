d7c2d096e414683962da56ad3e31913186b50f74
wqq0218 <wq0218@outlook.com>
2025-09-02 16:10:05 +0800

    Initial commit

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,10 +1,14 @@
 // 云函数入口文件
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV })
 
 exports.main = async (event, context) => {
+  // 获取微信上下文，其中包含 OPENID、APPID
+  const wxContext = cloud.getWXContext()
+  // 返回 OpenID 和 AppID
   return {
-    openid: context.OPENID
+    openid: wxContext.OPENID, // 使用此方法获取OPENID
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID, // 如果不在同一开放平台下，可能为空
   }
 }
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,6 @@
 App({
   onLaunch: function () {
-    wx.cloud.init();
+    wx.cloud.init({env: 'xly-3gf7enpg7be21056',  // 替换为实际环境 ID
+    traceUser: true,});
   }
 });
