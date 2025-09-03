7518cc3cececccda4599434159b53a3af6081ee7
Dale-X-Lee <Dale_Y_Z_Lee@qq.com>
2025-09-03 21:14:27 +0800

    注释掉了云函数中的日志，以保护信息

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -9,23 +9,23 @@ exports.main = async (event, context) => {
 
   // 查是否已有当前 openid
   var logging = 'none';
-  logging = 'Searching by openid: \n'+wxContext.OPENID+'\n';//日志，可删
+  // logging = 'Searching by openid: \n'+wxContext.OPENID+'\n';//日志，可删
   const existRes = await db.collection('people').where({
     _openid: wxContext.OPENID
   }).get()
 
   if (existRes.data.length > 0) {
     const user = existRes.data[0]
-    logging = logging+'user found:\nrole is: '+user.role+'\nid is: '+user._id+'\n';//日志，可删
+    // logging = logging+'user found:\nrole is: '+user.role+'\nid is: '+user._id+'\n';//日志，可删
     return { role: user.role, id: user._id, LogInfo: logging }
   }
 
-  logging = logging+'no openid found, checking people lists...\n\n'; // 日志，可删
+  // logging = logging+'no openid found, checking people lists...\n\n'; // 日志，可删
   // 查是否已有任何用户
   const allRes = await db.collection('people').get()
   if (allRes.data.length === 0) {
     // 第一个用户 → 管理员
-    logging=logging+'no people in the lists. adding admin...\n\n';//日志，可删
+    // logging=logging+'no people in the lists. adding admin...\n\n';//日志，可删
     const addRes = await db.collection('people').add({
       data: {
         name,
@@ -39,7 +39,7 @@ exports.main = async (event, context) => {
   }
 
   // 按姓名+电话匹配
-  logging=logging+'people list non empty, start searching by name and phone\nneme: '+name+'\nphone: '+phone+'\n';//日志，可删
+  // logging=logging+'people list non empty, start searching by name and phone\nneme: '+name+'\nphone: '+phone+'\n';//日志，可删
   const matchRes = await db.collection('people').where({
     name,
     phone
@@ -47,7 +47,7 @@ exports.main = async (event, context) => {
 
   if (matchRes.data.length > 0) {
     // 更新 openid
-    logging = logging+'people found, old openid is:\n'+matchRes.data[0]._openid+'\nand id is:\n'+matchRes.data[0]._id; //日志，可删
+    // logging = logging+'people found, old openid is:\n'+matchRes.data[0]._openid+'\nand id is:\n'+matchRes.data[0]._id; //日志，可删
     new_openid = wxContext.OPENID;
     await db.collection('people').doc(matchRes.data[0]._id).update({
       data: { _openid: new_openid }
@@ -60,10 +60,9 @@ exports.main = async (event, context) => {
   }
 
   // 没匹配到
-  logging=logging+'no one found!\n\n';//日志，可删
+  // logging=logging+'no one found!\n\n';//日志，可删
   return {
     role: 'none',
     LogInfo: logging
   }
 }
-
